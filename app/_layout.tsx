2cd1d90e1fa5dc27c8f79013d29c2f62c7dfc802
therealsamrajya <samrajyadai@gmail.com>
2024-11-18 13:45:15 +0545

    feat: create fixture section to get all city fixture BREAKING CHANGE : API integration from football-data.org

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,28 @@
-import { Stack } from "expo-router";
-import "../global.css"
+import { Tabs } from "expo-router";
+import "../global.css";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "../context/ThemeContext";
+import { Image } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider>
+      <SafeAreaProvider>
+        <Tabs screenOptions={{ headerShown: false }}>
+          <Tabs.Screen
+           options={{ title: "Home", tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../assets/images/mancity.png")} // Path to your custom image
+              style={{ width: size, height: size }} // Adjust size and color
+            />
+          ), }}
+            name="index"
+            />
+          <Tabs.Screen options={{ title: "FanVoice" }} name="FanVoice" />
+          <Tabs.Screen options={{ title: "Fixture" }} name="Fixture" />
+          
+        </Tabs>
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
 }
---
+++ constants/Colors.js
@@ -0,0 +1,7 @@
+export const Colors= {
+    light:{
+        text:'#FFFFFF',
+        background:"#6CABDD",
+        button:"#193F6E",
+    }
+}
