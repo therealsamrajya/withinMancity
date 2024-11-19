430962528e8740ae3533cbb648ce5816f2e58f11
therealsamrajya <samrajyadai@gmail.com>
2024-11-19 18:29:40 +0545

    feat : add constant data for card

---
+++ constants/data.js
@@ -0,0 +1,24 @@
+export const cardData = [
+    {
+      id: 1,
+      title: "Will Mancity Bounce Back?",
+      description: "After 4 defeats in a row and tough fixtures...",
+      fullDescription: "This is the full description of Card One. It provides more details about the card.",
+      image: require("@/assets/images/mancity1.jpg")
+    },
+    {
+      id: 2,
+      title: "Ballon'dor 2024 winner",
+      description: "Rodri has been named winner of this years ballon'dor",
+      fullDescription: "This is the full description of Card Two. It provides more details about the card.",
+      image: require("@/assets/images/rodri.jpg")
+    },
+    {
+      id: 3,
+      title: "Key Players Injured",
+      description: "More than 5 first team players to be absent...",
+      fullDescription: "This is the full description of Card Three. It provides more details about the card.",
+      image:require("@/assets/images/injury.jpg")
+    },
+  ];
+  
