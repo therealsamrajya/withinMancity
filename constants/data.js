1193307b41c702725f5824290999842a14514050
therealsamrajya <samrajyadai@gmail.com>
2024-11-21 16:09:04 +0545

    dependency update

--- constants/data.js
+++ constants/data.js
@@ -1,24 +1,26 @@
 export const cardData = [
-    {
-      id: 1,
-      title: "Will Mancity Bounce Back?",
-      description: "After 4 defeats in a row and tough fixtures...",
-      fullDescription: "This is the full description of Card One. It provides more details about the card.",
-      image: require("@/assets/images/mancity1.jpg")
-    },
-    {
-      id: 2,
-      title: "Ballon'dor 2024 winner",
-      description: "Rodri has been named winner of this years ballon'dor",
-      fullDescription: "This is the full description of Card Two. It provides more details about the card.",
-      image: require("@/assets/images/rodri.jpg")
-    },
-    {
-      id: 3,
-      title: "Key Players Injured",
-      description: "More than 5 first team players to be absent...",
-      fullDescription: "This is the full description of Card Three. It provides more details about the card.",
-      image:require("@/assets/images/injury.jpg")
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "Will Mancity Bounce Back?",
+    description: "After 4 defeats in a row and tough fixtures...",
+    fullDescription:
+      "Manchester City, known for their dominance in recent seasons, find themselves at a crossroads following an unexpected dip in form. With key fixtures ahead, fans and pundits alike are questioning whether the squad has the resilience to bounce back and reclaim their momentum. As Pep Guardiola looks to rally his players, all eyes are on the team's ability to respond under pressure and reassert their title credentials. Will City rise to the challenge, or are cracks beginning to show in their armor?",
+    image: require("@/assets/images/mancity1.jpg"),
+  },
+  {
+    id: 2,
+    title: "Ballon'dor 2024 winner",
+    description: "Rodri has been named winner of this years ballon'dor",
+    fullDescription:
+      "The 2024 Ballon d'Or winner in the men's category is Rodri, the Spanish midfielder from Manchester City. He had a phenomenal year, playing a pivotal role in Manchester City's historic treble victory (Premier League, FA Cup, and Champions League) and leading Spain to the UEFA Euro 2024 title. This marks the first time a Manchester City player has won this prestigious award. Rodri's consistent performances, including his critical Champions League-winning goal, made him a deserving winner​",
+    image: require("@/assets/images/rodri.jpg"),
+  },
+  {
+    id: 3,
+    title: "Key Players Injured",
+    description: "More than 5 first team players to be absent...",
+    fullDescription:
+      "Manchester City is currently grappling with a significant injury crisis, as key players like Kevin De Bruyne, Kyle Walker, Jack Grealish, and Jeremy Doku are sidelined. De Bruyne's thigh injury is expected to keep him out until after the November international break, while Walker is dealing with a knee issue. Grealish and Doku also face extended absences. In addition, defensive options like Manuel Akanji and Savinho are struggling with injuries, adding to the challenges. As the team faces a tough schedule across multiple competitions, Pep Guardiola's squad depth will be tested, with academy players potentially stepping up to fill the gaps",
+    image: require("@/assets/images/injury.jpg"),
+  },
+];
